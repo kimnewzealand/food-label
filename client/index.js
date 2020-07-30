1446b43b05cc9212b9629423558322341afd08fa
Kim Fitter <kimfitter@yahoo.com>
2020-07-30 14:42:16 +1200

    Views the form submission as a console log

--- client/index.js
+++ client/index.js
@@ -10,11 +10,11 @@ const store = createStore(reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-document.addEventListener('DOMContentLoaded', () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('app')
-  )
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   render(
+//     <Provider store={store}>
+//       {/* <App /> */}
+//     </Provider>,
+//     document.getElementById('app')
+//   )
+// })
