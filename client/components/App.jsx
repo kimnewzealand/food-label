fe6bb76521381547125116c6a74828928cb17d91
Kim Fitter <kimfitter@yahoo.com>
2020-08-03 17:26:57 +1200

    Set up the front end react components and react router

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { Component } from "react"
+import { Route} from "react-router-dom";
+import AddFood from './AddFood'
 
-const App = () => (
-  <div className='app-container'>
-<h1>Hello, let the project begin</h1>
-  </div>
-)
+const App = () => {
+  return (
+    <>
+        <h1> Welcome to the Food Label Data Viz Project!</h1>
+        <Route path="/addfood" exact component={AddFood} />
+    </>
+  )
+}
 
 export default App
--- client/index.js
+++ client/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import {render} from 'react-dom'
-import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import { render } from 'react-dom'
+import { HashRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import reducers from './reducers'
 import App from './components/App'
@@ -10,11 +11,14 @@ const store = createStore(reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   render(
-//     <Provider store={store}>
-//       {/* <App /> */}
-//     </Provider>,
-//     document.getElementById('app')
-//   )
-// })
+document.addEventListener('DOMContentLoaded', () => {
+  render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+
+    </Provider>,
+    document.getElementById('app')
+  )
+})
