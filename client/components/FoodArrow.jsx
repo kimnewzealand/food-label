9d33cd45e7b634d9c4b8b35b0e52032353ec59fa
Kim Fitter <kimfitter@yahoo.com>
2020-08-14 17:07:11 +1200

    Updates the layout of the foodarrow and welcome components

--- client/components/FoodArrow.jsx
+++ client/components/FoodArrow.jsx
@@ -6,12 +6,12 @@ const FoodArrow = () => {
   return (
     <>
         <div className="paper">
+            <div className="row flex-center">
+                <a href="https://www.heartfoundation.org.nz/wellbeing/healthy-eating/how-to-read-food-labels">The Heart Foundation NZ has guidance on how to read labels ...</a>
+            </div>
             <div className="container-xs">
                 <div className="row flex-center">
-                    <a href="https://www.heartfoundation.org.nz/wellbeing/healthy-eating/how-to-read-food-labels">The Heart Foundation NZ has guidance on how to read labels ...</a>
-                </div>
-                <div className="row flex-center">
-                    <img  width="40%" popover-top="Popover on top"
+                    <img  width="30%" popover-top="Popover on top"
                     src="https://www.heartfoundation.org.nz/media/images/nutrition/general-images/food-label-reading_737_553.jpg" alt=""/>
                 </div>
                 <div className="row flex-center">                  
@@ -23,3 +23,6 @@ const FoodArrow = () => {
   )
 }
 export default FoodArrow
+
+// interactive D3
+//https://towardsdatascience.com/react-d3-the-macaroni-and-cheese-of-the-data-visualization-world-12bafde1f922
--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -12,7 +12,7 @@ const Welcome = () => {
                 <h2>Welcome to a Food Label Data Viz Project!</h2>
               </div>
               <div className="text-center">
-                <Link to="/foodarrow" className="paper-bt margin"><h4>First let's take a look at a typical food label ➽</h4></Link>
+                <Link to="/foodarrow" className="paper-bt margin"><h4>Take a look at a typical food label ➽</h4></Link>
               </div>
             </div>
           </div>
