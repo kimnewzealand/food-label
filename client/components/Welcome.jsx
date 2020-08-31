ee0bc568bc33b878e1524ddb35767e7437c299c6
Kim Fitter <kimfitter@yahoo.com>
2020-09-01 10:49:42 +1200

    Add colours to welcome circles and react-spring animation component

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -1,30 +1,26 @@
 import React from "react"
 import { Link} from "react-router-dom"
 import * as d3 from 'd3'
-
-const circle_data = [50,200,400];
-const palette = d3.scaleOrdinal(d3.schemeSet3)
+import {useSpring, animated} from 'react-spring'
+import WelcomeCircles from './WelcomeCircles'
 
 const Welcome = () => {
+  const AnimatedWelcomeCircles = animated(WelcomeCircles)
+  // ...
+  const props = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+  })
   return (
     <>
-      {/* <div className="paper"> */}
+    {/* <div className="paper"> */}
         <div className="container-xs">
           <div id="top" class ="row site">
             <div className="row flex-center">
               <div className="text-center">
-                <h2>Welcome to a Food Label Data Viz Project!</h2>
-                <svg width="300" height="80">
-                  { circle_data.map((a, i) => (
-                    <circle
-                      key={i}
-                      cx={(i * 100) + 30}
-                      cy="50"
-                      r={Math.sqrt(a)}
-                      style={{ fill:  palette() }}
-                    />
-                  ))}
-              </svg>
+              <animated.div scrollTop={props.scroll} />
+              <h2>Welcome to a Food Label Data Viz Project!</h2>
+                <AnimatedWelcomeCircles opacity={props.opacity} />
               </div>
               <div className="text-center">
                 <Link to="/foodarrow" className="paper-bt margin"><h4>Take a look at a typical food label ➽</h4></Link>
---
+++ client/components/WelcomeCircles.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { Link} from "react-router-dom"
+import * as d3 from 'd3'
+
+const circle_data = [50,200,400];
+const palette = d3.scaleOrdinal(d3.schemeSet3) // color palette
+    
+const WelcomeCircles = () => {
+  return (
+    <>
+        <svg width="300" height="80">
+            { circle_data.map((a, i) => (
+                <circle
+                    key={i}
+                    cx={(i * 100) + 30}
+                    cy="50"
+                    r={Math.sqrt(a)}
+                    fill= {  palette(i) }
+                    opacity ="0.5"
+                />
+            ))}
+        </svg>
+              
+    </>
+  )
+}
+export default WelcomeCircles
